357f14541c764537c8ff2791ad78a116a146c69b
Jdesaof <daniiljanot@gmail.com>
2026-09-09 11:35:11 +0300

    Add arrays examples

--- index.php
+++ index.php
@@ -1,6 +1,36 @@
-
-<h1>
 <?php
-echo "<h3>Kakashki</h3>";
+/*$numbers = array(1,2,3); //old way
+$numbers = [1,2,3]; //new way
+$numbers[2] = 5;   
+var_dump($numbers[2]);
+$test = [1, 'test', true, [1, 2, 3]];
+var_dump($test);
+var_dump($test[3][1]);
+*/
+$test = [
+    'name' => 'Papa',
+    'age' => 27,
+    'city' => 'Pizzeria',
+    1,
+    2,
+    3,
+    'color' => 'brown',
+    100 => 6,
+    7,
+];
+var_dump($test);
+
+$combined = [1,2,3] + [4,5,6,7];
+var_dump($combined);
+//var_dump($test['name']);
+
+$text = implode('-', $test);
+var_dump($text);
+
+array_push($test, 67, 'Poopoo');
+var_dump($test);
+
+$test[] = 'new value';
+unset($test[100]);
+var_dump($test);
 ?>
-</h1>
