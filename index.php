b8a41b04448aa0343d0f80edd0afa88a4f984af5
Jdesaof <daniiljanot@gmail.com>
2026-09-09 10:36:55 +0300

    Add html example

--- index.php
+++ index.php
@@ -1,15 +1,6 @@
+
+<h1>
 <?php
-$test = "Govno";
-$test = "Bububu" . "Bebebe";
-$test .= "Rowr";
-$name = "Churka";
-$age = 6;
-$test = $name. ' is ' . $age . ' years old';
-$test = "$name is $age years old";
-$test = "lele\n\nlala";
-$test = <<<END
-cool stuff here
-multi line
-END;
-var_dump($test);
+echo "<h3>Kakashki</h3>";
 ?>
+</h1>
