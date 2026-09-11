d00cf4bdd5d0e04703881d7fe9182a69d5319efc
Jdesaof <daniiljanot@gmail.com>
2026-09-11 09:17:26 +0300

    add function example

--- index.php
+++ index.php
@@ -1,36 +1,29 @@
 <?php
-/*$numbers = array(1,2,3); //old way
-$numbers = [1,2,3]; //new way
-$numbers[2] = 5;   
-var_dump($numbers[2]);
-$test = [1, 'test', true, [1, 2, 3]];
-var_dump($test);
-var_dump($test[3][1]);
-*/
-$test = [
-    'name' => 'Papa',
-    'age' => 27,
-    'city' => 'Pizzeria',
-    1,
-    2,
-    3,
-    'color' => 'brown',
-    100 => 6,
-    7,
-];
-var_dump($test);
-
-$combined = [1,2,3] + [4,5,6,7];
-var_dump($combined);
-//var_dump($test['name']);
-
-$text = implode('-', $test);
-var_dump($text);
-
-array_push($test, 67, 'Poopoo');
-var_dump($test);
-
-$test[] = 'new value';
-unset($test[100]);
-var_dump($test);
+ 
+function hello(): void {
+    var_dump('hello');
+}
+ 
+var_dump(hello());
+hello();
+ 
+function helloname($name = 'Nameless', $age = 0) {
+    var_dump("hello, $name! You are $age years old");
+}
+ 
+helloname('misha', 18);
+ 
+function square(int $a): ?int {
+    if ($a < 0) {
+        return null;
+    }
+    return $a * $a;
+ 
+}
+ 
+$answer = square(-4);
+$answer = square(4);
+var_dump($answer);
+var_dump(square(5));
+ 
 ?>
