3059e8f37bbefda3a1006b6d4a76397ef4a1f2a2
Jdesaof <daniiljanot@gmail.com>
2026-09-23 10:17:29 +0300

    Add traits example

--- index.php
+++ index.php
@@ -1,47 +1,60 @@
 <?php
-
- class Box {
-    public int $width;
-    private int $height;
-    protected int $length;
-    
+ 
+class box {
+    use HasColor, HasSmell;
+    public $width;
+    public $height;
+    public $length;
+ 
     public function volume() {
-        return $this->width * $this->length * $this->height;
+        return $this->width * $this->height * $this->length;
     }
+ 
     public function setHeight(int $height) {
-        if ($height > 0) {
+        if ($height < 0) {
             $this->height = 0;
         } else {
             $this->height = $height;
         }
     }
-    public function getHeight() {
+    public function getHeight(int $height) {
         return $this->height;
     }
-
-    public function test1() {
-    $this->length = 10;
-    var_dump($this->length);
- }
-
- }
-
-class MetalBox extends Box {
+ 
+        public function test2() {
+        $this->length = 10;
+        var_dump($this->length);
+    }
+}
+ 
+class MetalBox extends box {
+    use HasColor;
     public $weightPerUnit;
     public function mass() {
-        return $this->volume() * $this->weightPerUnit;
+        return $this->weightPerUnit * $this->volume();
     }
-
-    public function test2() {
-    $this->height = 10;
-    var_dump($this->height);
- }
+        public function test2() {
+        $this->height = 10;
+        var_dump($this->height);
+    }
 }
-
-$box1 = new Box();
-$box1->length = 10;
-var_dump($box1->length);
-
-$metal1 = new MetalBox();
-var_dump($metal1);
-?> 
+ 
+trait HasColor {
+    public $color;
+    public function setColor(string $color) {
+        $this->color = $color;
+    }
+}
+ 
+trait HasSmell {
+    public $smell;
+    public function setSmell(string $smell) {
+        $this->smell = $smell;
+    }
+}
+ 
+$box = new box();
+var_dump($box);
+$metall = new MetalBox();
+var_dump($metall);
+?>
